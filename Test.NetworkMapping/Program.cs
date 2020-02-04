98c4601a607a2fa5da3c63a1606a4b9852e62a02
joão gabriel mauricio mauricio <joao.gabriel@builtcode.com.br>
2020-02-04 10:09:30 -0300

    first commit

---
+++ Test.NetworkMapping/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using SharpCifs.Smb;
+
+namespace Test.NetworkMapping
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var folder = new SmbFile("smb://192.168.0.114/planilhas/", new NtlmPasswordAuthentication("admin:@Tiva100"));
+
+            foreach (SmbFile f in folder.ListFiles())
+            {
+                Console.WriteLine(f.GetName());
+            }
+
+                //using (new ConnectToSharedFolder(@"\\server1\planilhas", new NetworkCredential(@"{admin}", "{@Tiva100}")))
+                //{
+                //    var FileList = Directory.GetDirectories(@"\\server1\planilhas");
+
+                //    foreach (var f in FileList)
+                //    {
+                //        Console.WriteLine(f);
+                //    }
+                //}
+
+
+            }
+    }
+}
+
